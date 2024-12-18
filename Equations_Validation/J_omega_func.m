f4f683fb4fad959dfa86e6a26ff54c378b1e2110
Edoardo Giacardi <edoardogiaca2001@gmail.com>
2024-12-18 14:21:18 +0100

    Delete Equations_Validation directory

--- Equations_Validation/J_omega_func.m
+++
@@ -1,6 +0,0 @@
-function J_omega = J_omega_func(in1)
-t1 = in1(1,:);
-t2 = cos(t1);
-t3 = sin(t1);
-J_omega = reshape([1.0,0.0,0.0,0.0,t2,t3,0.0,t2,t3],[3,3]);
-
--- Equations_Validation/skew.m
+++
@@ -1,5 +0,0 @@
-function X =skew(x)
-X=[0 -x(3) x(2) ; x(3) 0 -x(1) ; -x(2) x(1) 0 ];
-end
-% generates the antisymmetric matrix starting from a vector x=[x1;x2;x3] for
-% being able to perform the vector product between two vectors e.g. u x v
